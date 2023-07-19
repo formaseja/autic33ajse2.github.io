235d71c587ed7ef815749852a7987ab56767d5d0
formaseja <formaseja@gmail.com>
2023-07-18 19:12:38 -0600

    cambios

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDsUpL8uR1Zr0et6koLnPOtFTSUNHd8wbI",
+  authDomain: "autic33ase.firebaseapp.com",
+  projectId: "autic33ase",
+  storageBucket: "autic33ase.appspot.com",
+  messagingSenderId: "1010558580925",
+  appId: "1:1010558580925:web:b3db0c22cadca5f8ac9fd4",
+  measurementId: "G-M1RWBPEEQT"
+});
